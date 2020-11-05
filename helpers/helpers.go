866755c33cd337341800334f3e70188c67f30e26
Aja <a.g.mechanix@gmail.com>
2020-11-05 14:09:41 -0500

    setting up and creating validation func

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,12 +1,15 @@
 package helpers
 
 import (
+	"golang-backend/interfaces"
+	"regexp"
+
 	"github.com/jinzhu/gorm"
-	_"github.com/lib/pq"
+	_ "github.com/lib/pq"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func HandleErr(err error)  {
+func HandleErr(err error) {
 	if err != nil {
 		panic(err.Error())
 	}
@@ -21,8 +24,31 @@ func HashAndSalt(pass []byte) string {
 
 func ConnectDB() *gorm.DB {
 	db, err := gorm.Open(
-		"postgres", 
+		"postgres",
 		"host=127.0.0.1 port=5432 user=postgres dbname=bankapp	password=postgres sslmode=disable")
 	HandleErr(err)
 	return db
+}
+
+func Validation(values []interfaces.Validation) bool {
+	username := regexp.MustCompile(`^([A-za-z0-9{5,}])+$`)
+	email := regexp.MustCompile(`^[A-Za-z0-9]+[@]+[A-Za-z0-9]+[.]+[A-Za-z]+$`)
+
+	for i := 0; i < len(values); i++ {
+		switch values[i].Valid {
+		case "username":
+			if !username.MatchString(values[i].Value) {
+				return false
+			}
+		case "email":
+			if !email.MatchString(values[i].Value) {
+				return false
+			}
+		case "password":
+			if len(values[i].Value) < 5 {
+				return false
+			}
+		}
+	}
+	return true
 }
--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -5,27 +5,32 @@ import "github.com/jinzhu/gorm"
 type User struct {
 	gorm.Model
 	Username string
-	Email string
+	Email    string
 	Password string
 }
 
 type Account struct {
 	gorm.Model
-	Type string
-	Name string
+	Type    string
+	Name    string
 	Balance uint
-	UserID uint
+	UserID  uint
 }
 
 type ResponseAccount struct {
-	ID uint
-	Name string
+	ID      uint
+	Name    string
 	Balance uint
 }
 
 type ResponseUser struct {
-	ID uint
+	ID       uint
 	Username string
-	Email string
+	Email    string
 	Accounts []ResponseAccount
+}
+
+type Validation struct {
+	Value string
+	Valid string
 }
