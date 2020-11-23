3804b9f08d76473c56598e9ea02fb3f121fe1e75
Aja <a.g.mechanix@gmail.com>
2020-11-22 19:46:34 -0500

    add & refactored in readyBody & apiResponse func

--- api/api.go
+++ api/api.go
@@ -30,41 +30,32 @@ type ErrResponse struct {
 
 func register(w http.ResponseWriter, r *http.Request) {
 	// Read body
-	body, err := ioutil.ReadAll(r.Body)
-	helpers.HandleErr(err)
+	body := readBody(r)
 
 	// Handle registration
 	var formattedBody Register
 	err = json.Unmarshal(body, &formattedBody)
 	helpers.HandleErr(err)
+
 	register := users.Register(formattedBody.Username, formattedBody.Email, formattedBody.Password)
+	// Refactor register to use apiRespons
+	apiResponse(register, w)
 
 	// Prepare response
-	if register["message"] == "all is fine" {
-		resp := register
-		json.NewEncoder(w).Encode(resp)
-	} else { //handle err
-		resp := ErrResponse{Messahe: "Wrong username or password"}
-		json.NewEncoder(w).encode(resp)
-	}
+	
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	helpers.HandleErr(err)
+	// Refactor login to use readBody
+	body := readBody(r)
 
 	var formattedBody Login
 	err = json.Unmarshal(body, &formattedBody)
 	helpers.HandleErr(err)
+	
 	login := users.Login(formattedBody.Username, formattedBody.Password)
-
-	if login["message"] == "all is fine" {
-		resp := login
-		json.NewEncoder(w).Encode(resp)
-	} else {
-		resp := ErrResponse{Message: "Wrong username or password"}
-		json.NewEncoder(w).Encode(resp)
-	}
+	// Refactor login to use apiResponse
+	apiResponse(login, w)
 }
 
 func StartApi() {
@@ -73,4 +64,18 @@ func StartApi() {
 	router.HandleFunc("/register", register).Methods("POST")
 	fmt.Println("App is working on port :8888")
 	log.Fatal(http.ListenAndServe(":8888", router))
+}
+
+func readBody(r *http.Request) []byte {
+	body, err := ioutil.ReadAll(r.Body)
+	helpers.HandleErr(err)
+
+	return body
+}
+
+func apiResponse(call map[string]interface{}, w http.ResponseWriter) {
+	if call["message"] == "all is fine" {
+		resp := call
+		json.NewEncoder(w).Encode(resp)
+	}
 }
